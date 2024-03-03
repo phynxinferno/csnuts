accc7af3fa458cbafc66f23028ad0216b9db4a12
Phoenix <phoenixwb@netc.eu>
2024-03-03 00:52:21 -0500

    Card myself 2

--- build/main.cpp
+++ build/main.cpp
@@ -1,106 +1,133 @@
 //
-// Created by phoenix on 10/01/24.
+// Created by phoenix on 02/03/24.
 //
 
-//the horse is here(merge sort)
-
-// only works in powers of 2 but is set up for implementing compatibility with other numbers later :3
-
-/*
- * Accompanying Javascript tester functions
-
-function randomListButReal(ln) {
-	for(var list = ""; ln > 0; ln--)
-		list += (Math.round(Math.random() * 1000) + " ");
-	console.log(list);
-}
-
-function check(output) {
-// add space to end of output before running
-	let current = "";
-	let last;
-	for(let i = 0; i < output.length; i++) {
-		if(output.charAt(i) === ' ') {
-			if(parseInt(current) < parseInt(last)) return false;
-			last = current;
-			current = "";
-		} else {
-			current += ("" + output.charAt(i));
-		}
-	}
-	return true;
-}
-
- */
-
-#include <iostream>
-#include <cmath>
+#include "string"
 
 using namespace std;
 
+namespace cardSystem {
 
-int getLine() {
-    int line;
-    cin >> line;
-    return line;
+    const string faces[] = {"jester","jack", "queen", "king", "ace"};
+    const string suits[] = {"suitles","diamond", "club", "spade", "heart"};
+
+    class Card {
+    public:
+        unsigned int face, suit;
+        Card(const unsigned int* face, const unsigned int* suit) {
+            this->face = *face;
+            this->suit = *suit;
+        }
+        Card() {
+            face = (suit = 0);
+        };
+        Card(const Card &NewCard) {
+            *this = NewCard;
+        };
+        string toString() const {
+            return ((face > 4) ? to_string(face - 3) : faces[face]) + " of " + suits[suit] + "s";
+        }
+    };
+
+    ostream& operator<<(ostream& stream, const Card &logged) {
+        stream << logged.toString();
+        return stream;
+    }
+
+    Card BaseDeck[52];
+
+    class Deck {
+    public:
+        static const unsigned int sizeOfDeck = 52;
+        unsigned int currentCard = 0;
+        Card* Contents[52] = {};
+        bool moreCards() {
+            return currentCard > 50;
+        };
+        Deck() {
+            for(unsigned int i = 0; i < sizeOfDeck; i++) Contents[i] = &BaseDeck[i];
+        };
+        Card* DealCard() {
+            return Contents[(currentCard++)];
+        }
+        void shuffle() {
+            for(unsigned int i = 0; i < sizeOfDeck; i++) {
+                const unsigned int randPos = rand() % (sizeOfDeck - 1);
+                swap(Contents[i], Contents[randPos + (int)(randPos >= i - 1)]);
+            };
+        }
+    };
+
+}
+
+using namespace cardSystem;
+
+#include "iostream"
+#include "chrono"
+#include "vector"
+#include <cstring>
+
+string getLine() {
+    string input;
+    cin >> input;
+    return input;
+}
+
+void level3() {
+    Deck* Cards = new Deck();
+    Cards->shuffle();
+    for(unsigned int i = 0; i < Cards->sizeOfDeck; i++) {
+        cout << *Cards->DealCard() << endl;
+    }
+    delete Cards;
+}
+
+const Card* ReadErase(vector<Card*>* vect, const unsigned int index) {
+    const Card* Erased = vect->at(index);
+    vect->erase(vect->begin() + index);
+    return Erased;
+}
+
+void level4() {
+    cout << "Poker Game: Blackjack" << endl << "🗘 Shuffling deck..." << endl;
+    Deck* TempDeck = new Deck();
+    TempDeck->shuffle();
+    vector<Card*> BjDeck;
+    BjDeck.insert(BjDeck.end(),&TempDeck->Contents[0], &TempDeck->Contents[52]);
+    delete TempDeck;
+    cout << "🗊 Dealing..." << endl;
+    const Card* DealerHand[] = { ReadErase(&BjDeck,rand() % 52),ReadErase(&BjDeck,rand() % 51)};
+    cout << "🃟 The dealer's hand contains the hole and the " << *DealerHand[0] << "." << endl;
+    cout << "🃟͢ The " << *ReadErase(&BjDeck, 0) << " was removed from the deck." << endl;
+    unsigned int playerHand = 0;
+    do {
+        cout << "🏛 Your hand is currently worth " << playerHand << endl << "\tWould you like to [D]raw or [S]tand? ";
+        if(BjDeck.size() != 0 && tolower(getLine()[0]) != 's') {
+            const Card* Drawn = ReadErase(&BjDeck, rand() % BjDeck.size());
+            if(Drawn->face == 4) playerHand += (playerHand > 10) ? 1 : 11;
+            else playerHand += (Drawn->face > 4) ? Drawn->face - 3 : 10;
+            cout << "🃟̟ You drew the " << *Drawn << endl;
+            if(playerHand >= 21) {
+                cout << ((playerHand == 21) ? "💰 Twenty-one! You win." : "☠ Bust! The house wins.") << endl;
+                exit(0);
+            }
+        } else break;
+    } while(true);
+    cout << "🯅͏ You stood..." << endl << "🃟 The dealer had the " << *DealerHand[0] << " and the hole was " << *DealerHand[1] << "!" << endl;
+    unsigned int dealerHand = 0;
+    for(const Card* DealerCard : DealerHand) {
+        if(DealerCard->face == 4) dealerHand += 11;
+        else dealerHand += (DealerCard->face > 4) ? DealerCard->face - 3 : 10;
+    }
+    cout << ((playerHand > dealerHand) ? "💰 You win." : "☠ The house wins.") << endl;
 }
 
 int main(int argc, char** argv) {
-    // array-fetching stuff. not important.
-    const int length = getLine();
-    int arr[length];
-    for(int i = 0; i < length; i++) {
-        cin >> arr[i];
+    unsigned int offset;
+    srand(chrono::system_clock::now().time_since_epoch().count());
+    for(unsigned int i = 1; i < 5; i++) {
+        offset = (i - 1) * 13;
+        for(unsigned int j = 1; j < 14; j++) BaseDeck[offset + j - 1] = Card(&j,&i);
     }
-
-    // declare variables(explained later)
-    int subperiod, bottomPosition, topPosition;
-
-    // iterate over each period size until the one after it reaches >= length and declare subperiod
-    for(int period = 2; ceil(length / (double) (subperiod = period/2)) != 1; period *= 2) {
-
-        // iterate over each period(i represents period number)
-        for(int i = 0; i < ceil(length / (double) period); i++) {
-            // set starting positions to merge into(second subperiod)
-                topPosition = (bottomPosition = period * i) + subperiod -1;
-
-                //iterate over elements of the second subperiod;
-                for(int k = period * i + subperiod; k < period * (i+1); k++) {
-                    // iterate over elements in first subperiod starting from the end
-                    for(int j = topPosition; true; j--) {
-
-                        // swap the element with the one after it if it's greater, starting with the element of the second subperiod's original position(the element after will always be the value of the original element of the second subperiod)
-                        if(arr[j] > arr[j+1]) swap(arr[j], arr[j+1]);
-
-                        // if the element hasn't hit the bottom position but is in the right spot, move the bottom position to after it(element after it in its subperiod will always be greater)
-                        else if(arr[j + 1] != arr[k]) {
-                                bottomPosition = j + 2;
-                                // increment top position to account for shift
-                                topPosition++;
-
-                                break;
-                        }
-
-                        // if it's hit the bottom position, move the bottom position to after it in a simpler manner.
-                        if(j == bottomPosition) {
-                            bottomPosition++;
-                            // increment top position to account for shift
-                            topPosition++;
-
-                            break;
-                        }
-
-                    }
-
-                }
-
-            }
-
-
-    }
-
-    // print array :3
-    for(int ele : arr) cout << ele << " ";
-
+    level4();
 }
-
--- card/Card.cpp
+++ card/Card.cpp
@@ -2,36 +2,37 @@
 // Created by phoenix on 02/03/24.
 //
 
-#include "Card.h"
 #include "string"
 
 using namespace std;
 
 namespace cardSystem {
 
-    const string faces[] = {"Jester","Jack", "Queen", "King"};
+    const string faces[] = {"jester","jack", "queen", "king", "ace"};
     const string suits[] = {"suitles","diamond", "club", "spade", "heart"};
 
     class Card {
     public:
         unsigned int face, suit;
-        Card(const unsigned int face, const unsigned int suit) {
-            this->face = face;
-            this->suit = suit;
+        Card(const unsigned int* face, const unsigned int* suit) {
+            this->face = *face;
+            this->suit = *suit;
         }
         Card() {
             face = (suit = 0);
         };
-        Card(const Card &card) {
-            *this = card;
+        Card(const Card &NewCard) {
+            *this = NewCard;
         };
-        string toString() {
+        string toString() const {
             return ((face > 4) ? to_string(face - 3) : faces[face]) + " of " + suits[suit] + "s";
         }
-        ostream& operator<<(ostream& stream) {
-            stream << toString();
-        }
     };
+
+    ostream& operator<<(ostream& stream, const Card &logged) {
+        stream << logged.toString();
+        return stream;
+    }
 
     Card BaseDeck[52];
 
@@ -40,7 +41,6 @@ namespace cardSystem {
         static const unsigned int sizeOfDeck = 52;
         unsigned int currentCard = 0;
         Card* Contents[52] = {};
-        Card* deal();
         bool moreCards() {
             return currentCard > 50;
         };
@@ -48,7 +48,7 @@ namespace cardSystem {
             for(unsigned int i = 0; i < sizeOfDeck; i++) Contents[i] = &BaseDeck[i];
         };
         Card* DealCard() {
-            return Contents[(currentCard++) - 1];
+            return Contents[(currentCard++)];
         }
         void shuffle() {
             for(unsigned int i = 0; i < sizeOfDeck; i++) {
@@ -58,4 +58,76 @@ namespace cardSystem {
         }
     };
 
+}
+
+using namespace cardSystem;
+
+#include "iostream"
+#include "chrono"
+#include "vector"
+#include <cstring>
+
+string getLine() {
+    string input;
+    cin >> input;
+    return input;
+}
+
+void level3() {
+    Deck* Cards = new Deck();
+    Cards->shuffle();
+    for(unsigned int i = 0; i < Cards->sizeOfDeck; i++) {
+        cout << *Cards->DealCard() << endl;
+    }
+    delete Cards;
+}
+
+const Card* ReadErase(vector<Card*>* vect, const unsigned int index) {
+    const Card* Erased = vect->at(index);
+    vect->erase(vect->begin() + index);
+    return Erased;
+}
+
+void level4() {
+    cout << "Poker Game: Blackjack" << endl << "🗘 Shuffling deck..." << endl;
+    Deck* TempDeck = new Deck();
+    TempDeck->shuffle();
+    vector<Card*> BjDeck;
+    BjDeck.insert(BjDeck.end(),&TempDeck->Contents[0], &TempDeck->Contents[52]);
+    delete TempDeck;
+    cout << "🗊 Dealing..." << endl;
+    const Card* DealerHand[] = { ReadErase(&BjDeck,rand() % 52),ReadErase(&BjDeck,rand() % 51)};
+    cout << "🃟 The dealer's hand contains the hole and the " << *DealerHand[0] << "." << endl;
+    cout << "🃟͢ The " << *ReadErase(&BjDeck, 0) << " was removed from the deck." << endl;
+    unsigned int playerHand = 0;
+    do {
+        cout << "🏛 Your hand is currently worth " << playerHand << endl << "\tWould you like to [D]raw or [S]tand? ";
+        if(BjDeck.size() != 0 && tolower(getLine()[0]) != 's') {
+            const Card* Drawn = ReadErase(&BjDeck, rand() % BjDeck.size());
+            if(Drawn->face == 4) playerHand += (playerHand > 10) ? 1 : 11;
+            else playerHand += (Drawn->face > 4) ? Drawn->face - 3 : 10;
+            cout << "🃟̟ You drew the " << *Drawn << endl;
+            if(playerHand >= 21) {
+                cout << ((playerHand == 21) ? "💰 Twenty-one! You win." : "☠ Bust! The house wins.") << endl;
+                exit(0);
+            }
+        } else break;
+    } while(true);
+    cout << "🯅͏ You stood..." << endl << "🃟 The dealer had the " << *DealerHand[0] << " and the hole was " << *DealerHand[1] << "!" << endl;
+    unsigned int dealerHand = 0;
+    for(const Card* DealerCard : DealerHand) {
+        if(DealerCard->face == 4) dealerHand += 11;
+        else dealerHand += (DealerCard->face > 4) ? DealerCard->face - 3 : 10;
+    }
+    cout << ((playerHand > dealerHand) ? "💰 You win." : "☠ The house wins.") << endl;
+}
+
+int main(int argc, char** argv) {
+    unsigned int offset;
+    srand(chrono::system_clock::now().time_since_epoch().count());
+    for(unsigned int i = 1; i < 5; i++) {
+        offset = (i - 1) * 13;
+        for(unsigned int j = 1; j < 14; j++) BaseDeck[offset + j - 1] = Card(&j,&i);
+    }
+    level4();
 }
